a8a17ff1acb32956f4ec739b103ec6c27440f8f7
Galaxy-B <1146948671@qq.com>
2025-01-19 22:41:31 +0800

    :sparkles: util func 'alignUpPowOf2'

---
+++ lfcq/utils.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#include <stdint.h>
+#include <algorithm>
+
+namespace lfcq {
+
+#define QUEUE_MAX_SIZE 0x8000'0000U
+
+/* align the input UP to power of 2 */
+inline uint32_t alignUpPowOf2(uint32_t val) {
+    if (val == 0) return 1;
+
+    // in case overflow or val is already power of 2
+    val = std::max(val, QUEUE_MAX_SIZE) - 1;
+    val |= val >> 1;
+    val |= val >> 2;
+    val |= val >> 4;
+    val |= val >> 8;
+    val |= val >> 16;
+
+    return val + 1;
+}
+
+#undef QUEUE_MAX_SIZE
+
+}  // namespace lfcq
