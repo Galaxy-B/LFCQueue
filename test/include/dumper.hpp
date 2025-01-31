239f510f4cd623bd3beb49f8ba47b25e73ced762
Galaxy-B <1146948671@qq.com>
2025-02-01 00:40:37 +0800

    :art: name of Dumper's private variable

--- test/include/dumper.hpp
+++ test/include/dumper.hpp
@@ -10,21 +10,21 @@ using Path = std::filesystem::path;
 /* make it easier to manage file stream */
 class Dumper {
   private:
-    std::ofstream file;
+    std::ofstream file_;
 
   public:
-    explicit Dumper(const std::string&& path) : file(Path(path)) {
-        if (!file) {
+    explicit Dumper(const std::string&& path) : file_(Path(path)) {
+        if (!file_) {
             std::cout << "fail to open or create dump file: " << path << std::endl;
             exit(1);
         }
     }
 
-    ~Dumper() { file.close(); }
+    ~Dumper() { file_.close(); }
 
     template <typename... Args>
     void dump(Args&&... args) {
-        (file << ... << std::forward<Args>(args)) << std::endl;
+        (file_ << ... << std::forward<Args>(args)) << std::endl;
     }
 };
 
